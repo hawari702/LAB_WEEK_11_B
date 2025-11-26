8707133cd9887a3525481798eeaa50196fa52f22
Nama Anda <emailpribadi@example.com>
2025-11-26 23:33:02 +0700

    Commit No. 3: Assignment

--- app/src/main/java/com/example/lab_week_11_b/MediaContentHelper.kt
+++ app/src/main/java/com/example/lab_week_11_b/MediaContentHelper.kt
@@ -1,30 +1,30 @@
-package com.example.lab_week_11_b
+    package com.example.lab_week_11_b
 
-import android.content.ContentValues
-import android.provider.MediaStore
+    import android.content.ContentValues
+    import android.provider.MediaStore
 
-class MediaContentHelper {
+    class MediaContentHelper {
 
-    fun getImageContentUri() = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-    fun getVideoContentUri() = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+        fun getImageContentUri() = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        fun getVideoContentUri() = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
 
-    fun generateImageContentValues(fileInfo: FileInfo): ContentValues {
-        val values = ContentValues()
+        fun generateImageContentValues(fileInfo: FileInfo): ContentValues {
+            val values = ContentValues()
 
-        values.put(MediaStore.Images.Media.DISPLAY_NAME, fileInfo.name)
-        values.put(MediaStore.Images.Media.MIME_TYPE, fileInfo.mimeType)
-        values.put(MediaStore.Images.Media.RELATIVE_PATH, getPicturesFolder())
+            values.put(MediaStore.Images.Media.DISPLAY_NAME, fileInfo.name)
+            values.put(MediaStore.Images.Media.MIME_TYPE, fileInfo.mimeType)
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, getPicturesFolder())
 
-        return values
+            return values
+        }
+
+        fun generateVideoContentValues(fileInfo: FileInfo): ContentValues {
+            val values = ContentValues()
+
+            values.put(MediaStore.Video.Media.DISPLAY_NAME, fileInfo.name)
+            values.put(MediaStore.Video.Media.MIME_TYPE, fileInfo.mimeType)
+            values.put(MediaStore.Video.Media.RELATIVE_PATH, getVideosFolder())
+
+            return values
+        }
     }
-
-    fun generateVideoContentValues(fileInfo: FileInfo): ContentValues {
-        val values = ContentValues()
-
-        values.put(MediaStore.Video.Media.DISPLAY_NAME, fileInfo.name)
-        values.put(MediaStore.Video.Media.MIME_TYPE, fileInfo.mimeType)
-        values.put(MediaStore.Video.Media.RELATIVE_PATH, getVideosFolder())
-
-        return values
-    }
-}
